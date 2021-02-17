6e788ef0f3d60acce08c18503d39ad646eff08f7
jona-git <75018238+jona-git@users.noreply.github.com>
2021-02-18 10:41:19 +1100

    ScoreController test

--- BowlingScorer/Bowling.Score.Calculator.Tests/ScorerControllerTests.cs
+++ BowlingScorer/Bowling.Score.Calculator.Tests/ScorerControllerTests.cs
@@ -1,14 +1,55 @@
-﻿using NUnit.Framework;
+﻿using Bowling.Score.Calculator.Controllers;
+using Bowling.Score.Calculator.Infrastructure.Exceptions;
+using Bowling.Score.Calculator.Model;
+using Bowling.Score.Calculator.Services;
+using Bowling.Score.Calculator.Validation;
+using NUnit.Framework;
+using System.Net.Http;
 
 namespace Bowling.Score.Calculator.Tests
 {
     [TestFixture]
-    public class ScorerControllerTests
+    public class ScorerControllerTests 
     {
-        [Test]
-        public void CalculateScoreTest()
+        private readonly IScoreCalculator scoreCalculator;
+        private readonly IGameValidator gameValidator;
+        private readonly ScorerController scorerController;
+        public ScorerControllerTests()
         {
+            gameValidator = new GameValidator(); //used actual instance for this since this is lightweight, otherwise will mock dependencies
+
+            scoreCalculator = new ScoreCalculator(gameValidator);
+            scorerController = new ScorerController(scoreCalculator);
+        }
+
+
+        [Test]
+        public void CalculatePerfectScoreTest()
+        {            
+            var result = scorerController.Post(ScoreTestHelper.PerfectScore());
+            Assert.AreEqual(result.GetType(), typeof(GameScore));
+            Assert.IsTrue(result.FrameCompleted);
+            Assert.AreEqual(result.Score, 300);
+        }
+
+
+        [Test]
+        public void CalculateGutterBallGameScoreTest()
+        {
+            var result = scorerController.Post(ScoreTestHelper.GutterBallScores());
+            Assert.AreEqual(result.GetType(), typeof(GameScore));
+            Assert.IsTrue(result.FrameCompleted);
+            Assert.AreEqual(result.Score, 0);
 
         }
+
+        [Test]
+        public void InvalidScoresTest()
+        {
+            Assert.Throws<ScorerException>(() => scorerController.Post(ScoreTestHelper.InvalidOpenFrameScoreGreaterThan10()));
+           
+        }
+
+
     }
 }
