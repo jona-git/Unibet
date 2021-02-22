42c87111af2cf628c701623aa4141c1328147b47
jona-git <75018238+jona-git@users.noreply.github.com>
2021-02-22 22:01:09 +1100

    Mocked in ScorerControllerTests
    Replaced actual instance to mocked  interface in ScorerControllerTests.

--- BowlingScorer/Bowling.Score.Calculator.Tests/ScorerControllerTests.cs
+++ BowlingScorer/Bowling.Score.Calculator.Tests/ScorerControllerTests.cs
@@ -2,7 +2,7 @@
 using Bowling.Score.Calculator.Infrastructure.Exceptions;
 using Bowling.Score.Calculator.Model;
 using Bowling.Score.Calculator.Services;
-using Bowling.Score.Calculator.Validation;
+using Moq;
 using NUnit.Framework;
 
 namespace Bowling.Score.Calculator.Tests
@@ -10,32 +10,35 @@ namespace Bowling.Score.Calculator.Tests
     [TestFixture]
     public class ScorerControllerTests 
     {
-        private readonly IScoreCalculator scoreCalculator;
-        private readonly IGameValidator gameValidator;
-        private readonly ScorerController scorerController;
-        public ScorerControllerTests()
-        {
-            gameValidator = new GameValidator(); //used actual instance for this since this is lightweight, otherwise will mock dependencies
-            scoreCalculator = new ScoreCalculator(gameValidator);
-
-            scorerController = new ScorerController(scoreCalculator);
-        }
-
-
+        
         [Test]
         public void CalculatePerfectScoreTest()
-        {            
-            var result = scorerController.Post(ScoreTestHelper.PerfectScore.TestScores);
+        {
+            var fakeResult = new GameScore() { FrameCompleted = true, Score = 300 };
+            var scoreCalculator = new Mock<IScoreCalculator>();
+            scoreCalculator.Setup(c => c.Calculate(It.IsAny<int[]>())).Returns(fakeResult).Verifiable();
+            var scorerController = new ScorerController(scoreCalculator.Object);
+            var result = scorerController.Post(ScoreTestHelper.PerfectScore.TestScores);         
+
+            scoreCalculator.Verify(s=>s.Calculate(It.IsAny<int[]>()), Times.Once());
             Assert.AreEqual(result.GetType(), typeof(GameScore));
             Assert.IsTrue(result.FrameCompleted);
             Assert.AreEqual(result.Score, ScoreTestHelper.PerfectScore.ExpectedTotalScore);
+            
         }
 
 
         [Test]
         public void CalculateGutterBallGameScoreTest()
-        {
+        {            
+            var fakeResult = new GameScore() { FrameCompleted = true, Score = 0 };
+            var scoreCalculator = new Mock<IScoreCalculator>();
+            scoreCalculator.Setup(c => c.Calculate(It.IsAny<int[]>())).Returns(fakeResult).Verifiable();
+            var scorerController = new ScorerController(scoreCalculator.Object);
             var result = scorerController.Post(ScoreTestHelper.GutterBallScores.TestScores);
+
+            scoreCalculator.Verify(s => s.Calculate(It.IsAny<int[]>()), Times.Once());
+            scoreCalculator.VerifyAll();
             Assert.AreEqual(result.GetType(), typeof(GameScore));
             Assert.IsTrue(result.FrameCompleted);
             Assert.AreEqual(result.Score, ScoreTestHelper.GutterBallScores.ExpectedTotalScore);
@@ -45,10 +48,19 @@ namespace Bowling.Score.Calculator.Tests
         [Test]
         public void InvalidScoresTest()
         {
+            var scoreCalculator = new Mock<IScoreCalculator>();
+            scoreCalculator.Setup(c => c.Calculate(It.IsAny<int[]>()))
+                .Throws(new ScorerException())
+                .Verifiable();
+            
+
+            var scorerController = new ScorerController(scoreCalculator.Object);
             Assert.Throws<ScorerException>(() => scorerController.Post(ScoreTestHelper.InvalidOpenFrameScoreGreaterThan10()));
-           
+            scoreCalculator.Verify(s => s.Calculate(It.IsAny<int[]>()), Times.Once());
+            
         }
 
+      
 
     }
 }
